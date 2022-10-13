752447b9eb0c55718aae9527b7f82a1088a95b8b
mugdha273 <mugdhasharma0327@gmail.com>
2022-10-13 16:47:24 +0530

    adds teacher and student models, registered in admin

--- ELearn/Student/models.py
+++ ELearn/Student/models.py
@@ -1,3 +1,13 @@
+from email.policy import default
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import date
 
 # Create your models here.
+class StudentModel(models.Model):
+    student = models.ForeignKey(User, on_delete=models.CASCADE)
+    joining_date = models.DateField(default=date.today)
+    student_idnum = models.IntegerField()
+    
+    def __str__(self):
+        return self.student.first_name + ' ' + self.student.last_name
--- ELearn/Teacher/models.py
+++ ELearn/Teacher/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import date
 
 # Create your models here.
+class TeacherModel(models.Model):
+    teacher = models.ForeignKey(User, on_delete=models.CASCADE)
+    joining_date = models.DateField(default=date.today)
+    teacher_idnum = models.IntegerField()
+    
+    def __str__(self):
+        return self.teacher.first_name + ' ' + self.teacher.last_name
+    
